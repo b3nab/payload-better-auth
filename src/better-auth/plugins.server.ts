e46f602c6dfed1428dc364339f51bb7b9fa13b93
Ben <b3nab@users.noreply.github.com>
2025-02-16 19:03:42 +0100

    feat: pluginsToLoad function which takes the plugin config object and create the array of plugins to be loaded by better-auth

---
+++ src/better-auth/plugins.server.ts
@@ -0,0 +1,80 @@
+import {
+  type BetterAuthPlugin,
+  // core authentication
+  twoFactor,
+  username,
+  anonymous,
+  phoneNumber,
+  magicLink,
+  emailOTP,
+  // passkey,
+  genericOAuth,
+  oneTap,
+  // core authorization
+  admin,
+  organization,
+  // core enterprise
+  oidcProvider,
+  // sso,
+  // core utility
+  bearer,
+  multiSession,
+  oAuthProxy,
+  openAPI,
+  jwt,
+} from 'better-auth/plugins'
+import { passkey } from 'better-auth/plugins/passkey'
+import { sso } from 'better-auth/plugins/sso'
+import type { BetterAuthPluginOptions } from '../index.js'
+
+export const defaultPlugins: BetterAuthPlugin[] = [
+  twoFactor(),
+  passkey(),
+  openAPI(),
+]
+export const pluginsToLoad = (
+  pluginOptions: BetterAuthPluginOptions,
+): BetterAuthPlugin[] =>
+  pluginOptions.betterAuthPlugins
+    ? Object.entries(pluginOptions.betterAuthPlugins)
+        .map(([key, plugin]) => {
+          if (typeof plugin === 'boolean') {
+            return {
+              // core authentication
+              twoFactor: twoFactor(),
+              username: username(),
+              anonymous: anonymous(),
+              phoneNumber: phoneNumber(),
+              // TODO: need to pass options for plugin
+              // magicLink: magicLink(),
+              // TODO: need to pass options for plugin
+              // emailOTP: emailOTP(),
+              passkey: passkey(),
+              // TODO: need to pass options for plugin
+              // genericOAuth: genericOAuth(),
+              oneTap: oneTap(),
+              // core authorization
+              admin: admin(),
+              organization: organization(),
+              // core enterprise
+              // TODO: need to pass options for plugin
+              // oidcProvider: oidcProvider(),
+              sso: sso(),
+              // core utility
+              bearer: bearer(),
+              multiSession: multiSession(),
+              oAuthProxy: oAuthProxy(),
+              openAPI: openAPI(),
+              jwt: jwt(),
+              // third-party
+              // TODO: need to pass options for plugin
+              // harmony: harmony(),
+              // TODO: need to pass options for plugin
+              // validator: validator(),
+            }[key]
+          }
+          return plugin()
+        })
+        // need to filter out undefined plugins
+        .filter((plugin) => !!plugin)
+    : defaultPlugins
