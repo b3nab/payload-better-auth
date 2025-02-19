8fd18279045e74ceb39ed2440fd8a48a93404022
Ben <b3nab@users.noreply.github.com>
2025-02-19 05:43:41 +0100

    fix: keep payload singleton fresh and update it inside every wrapper auth get, patch and post requests

--- src/payload-utilities/wrapInternalEndpoints.ts
+++ src/payload-utilities/wrapInternalEndpoints.ts
@@ -1,16 +1,27 @@
-import type { Endpoint } from 'payload'
+import type { Endpoint, PayloadHandler } from 'payload'
 
 import { addDataAndFileToRequest } from './addDataAndFileToRequest.js'
 import { addLocalesToRequestFromData } from './addLocalesToRequest.js'
+import { payloadSingleton } from '../singleton.payload.js'
 
 export const wrapInternalEndpoints = (endpoints: Endpoint[]): Endpoint[] => {
   return endpoints.map((endpoint) => {
     const handler = endpoint.handler
+    // let handler: PayloadHandler = endpoint.handler
+
+    if (['get'].includes(endpoint.method)) {
+      endpoint.handler = async (req) => {
+        console.log('method', endpoint.method)
+        payloadSingleton(req.payload)
+        return handler(req)
+      }
+    }
 
     if (['patch', 'post'].includes(endpoint.method)) {
       endpoint.handler = async (req) => {
         await addDataAndFileToRequest(req)
         addLocalesToRequestFromData(req)
+        payloadSingleton(req.payload)
         return handler(req)
       }
     }
