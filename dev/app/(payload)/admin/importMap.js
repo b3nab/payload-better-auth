85493f9e03999638502986631c83df6e26753e31
Ben <b3nab@users.noreply.github.com>
2025-02-20 22:55:54 +0100

    chore: update dev importMap

--- dev/app/(payload)/admin/importMap.js
+++ dev/app/(payload)/admin/importMap.js
@@ -1,5 +1,9 @@
-
+import { BetterAuthServerWrapper as BetterAuthServerWrapper_0814f4521f2b1f0fcc391051cb30e62b } from 'payload-better-auth/rsc'
+import { SetupTwoFactorServer as SetupTwoFactorServer_0814f4521f2b1f0fcc391051cb30e62b } from 'payload-better-auth/rsc'
+import { VerifyTwoFactorServer as VerifyTwoFactorServer_0814f4521f2b1f0fcc391051cb30e62b } from 'payload-better-auth/rsc'
 
 export const importMap = {
-
+  "payload-better-auth/rsc#BetterAuthServerWrapper": BetterAuthServerWrapper_0814f4521f2b1f0fcc391051cb30e62b,
+  "payload-better-auth/rsc#SetupTwoFactorServer": SetupTwoFactorServer_0814f4521f2b1f0fcc391051cb30e62b,
+  "payload-better-auth/rsc#VerifyTwoFactorServer": VerifyTwoFactorServer_0814f4521f2b1f0fcc391051cb30e62b
 }
