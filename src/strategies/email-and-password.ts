25ff5ed180975ba46d082d2be7e32ee8be93128b
Ben <b3nab@users.noreply.github.com>
2025-02-22 11:13:30 +0100

    chore: use logger into strategies and add _strategy and by params to user object

--- src/strategies/email-and-password.ts
+++ src/strategies/email-and-password.ts
@@ -1,6 +1,7 @@
 import type { AuthStrategyFunction } from 'payload'
 import { payloadSingleton } from '../singleton.payload.js'
 import { getBetterAuth } from '../singleton.better-auth.js'
+import { getLogger } from '../logger.js'
 
 export const emailAndPasswordStrategy: AuthStrategyFunction = async ({
   headers,
@@ -9,13 +10,18 @@ export const emailAndPasswordStrategy: AuthStrategyFunction = async ({
   strategyName,
 }) => {
   payloadSingleton(payload)
-  console.log('[better-auth] [strategy] [emailAndPassword]')
+  const logger = getLogger()
+  logger.trace('[server] [strategy] [emailAndPassword]')
   // console.log('emailAndPassword', headers)
   const betterAuth = getBetterAuth()
-  const result = await betterAuth?.api.getSession({
+  const response = await betterAuth?.api.getSession({
     headers: headers,
+    asResponse: true,
   })
-  console.log('getSession result', result)
+  const result = await response?.json()
+  logger.debug(
+    `[server] [strategy] [emailAndPassword] getSession result: ${!!result}`,
+  )
 
   const user = result?.user
     ? {
@@ -24,10 +30,13 @@ export const emailAndPasswordStrategy: AuthStrategyFunction = async ({
         id: result.user.id,
         email: result.user.email,
         // username: result.user.username,
+        _strategy: 'email-and-password',
+        by: 'strategy-email-and-password',
       }
     : null
 
   return {
     user: user ?? null,
+    // responseHeaders: response?.headers ?? new Headers(),
   }
 }
--- src/strategies/two-factor.ts
+++ src/strategies/two-factor.ts
@@ -3,6 +3,8 @@ import { payloadSingleton } from '../singleton.payload.js'
 import { getBetterAuth } from '../singleton.better-auth.js'
 import { cookies as nextCookies, headers as nextHeaders } from 'next/headers.js'
 import { redirect } from 'next/navigation.js'
+import { formatAdminURL } from '@payloadcms/ui/shared'
+import { getLogger } from '../logger.js'
 
 const userNull = (headers?: Headers) => {
   return {
@@ -18,12 +20,30 @@ export const twoFactorStrategy: AuthStrategyFunction = async ({
   strategyName,
 }) => {
   payloadSingleton(payload)
-  console.log('[better-auth] [strategy] [twoFactor]')
+  const logger = getLogger()
+  logger.trace('[server] [strategy] [twoFactor]')
 
   const cookies = await nextCookies()
-  const twoFactorSession = cookies.get('better-auth.two_factor')
+  const cookiesList = cookies.getAll()
+  const cookiesMap = cookiesList.reduce(
+    (acc, cookie) => {
+      acc[cookie.name] = cookie.value
+      return acc
+    },
+    {} as Record<string, string>,
+  )
+  const twoFactorSession = cookiesMap['better-auth.two_factor']
+  logger.debug(
+    {
+      twoFactorSession,
+    },
+    '[server] [strategy] [twoFactor] twoFactorSession',
+  )
+
+  const responseHeaders = new Headers(headers)
 
   if (twoFactorSession) {
+    responseHeaders.set('x-two-factor-redirect', 'true')
     // 2FA session found
     // TODO: need to find a way to verify that the 2fa session is valid
     return {
@@ -31,8 +51,14 @@ export const twoFactorStrategy: AuthStrategyFunction = async ({
         id: 'two-factor-id',
         email: 'two-factor-email',
         collection: payload.config.admin.user,
+        redirectTo: formatAdminURL({
+          adminRoute: payload.config.routes.admin,
+          path: '/two-factor-verify',
+        }),
+        _strategy: 'two-factor',
+        by: 'strategy-two-factor',
       },
-      responseHeaders: headers,
+      responseHeaders,
     }
     // const betterAuth = getBetterAuth()
     // const response = await betterAuth?.api.getSession({
