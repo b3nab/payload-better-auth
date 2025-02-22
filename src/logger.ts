34cc1011af83a930dc1c88517e9fd64acd79c7d6
Ben <b3nab@users.noreply.github.com>
2025-02-22 08:47:36 +0100

    feat: add pino and create a singleton logger instance

---
+++ src/logger.ts
@@ -0,0 +1,24 @@
+import { pino } from 'pino'
+import invariant from 'tiny-invariant'
+
+// Also pino should be initialized with the payload config so it has to use a singleton pattern
+
+let __logger: pino.Logger | undefined
+
+export const getLogger = () => {
+  invariant(__logger, 'Logger not initialized')
+  return __logger
+}
+
+export type LoggerConfig = {
+  level: pino.Level
+}
+
+export const initLogger = (config: LoggerConfig) => {
+  __logger = pino({
+    level: config.level,
+  })
+  return __logger
+}
+
+// export const logger = getLogger()
