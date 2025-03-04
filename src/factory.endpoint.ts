2f0a7a84b8a63a2299532f55b1a5a9026b999bfa
Ben <b3nab@users.noreply.github.com>
2025-03-04 15:52:39 +0100

    fix: endpoint factory - types are messed up since better-auth from v1.2.0 reduced the type overhead simplified them but now the types are screwd (it feels like old js days..)

--- src/factory.endpoint.ts
+++ src/factory.endpoint.ts
@@ -1,6 +1,7 @@
 import type { Endpoint, PayloadHandler } from 'payload'
 import type { betterAuth } from 'better-auth'
 import type { getEndpoints } from 'better-auth/api'
+import { payloadSingleton } from './singleton.payload.js'
 
 type BetterAuthInstance = ReturnType<typeof betterAuth>
 type AuthEndpointsApi = ReturnType<typeof getEndpoints>['api']
@@ -12,7 +13,7 @@ export class EndpointFactory {
     betterAuthInstance: ReturnType<typeof betterAuth>,
     betterAuthPaths: AuthEndpointsApi,
   ) {
-    // console.log('keys betterAuthPaths:', Object.keys(betterAuthPaths))
+    // console.log('keys betterAuthPaths:', betterAuthPaths)
     this.betterAuthInstance = betterAuthInstance
     this.betterAuthPaths = betterAuthPaths
   }
@@ -22,26 +23,34 @@ export class EndpointFactory {
     return Object.entries(this.betterAuthPaths)
       .filter((authPath) => {
         const [key, value] = authPath
-        // console.log('KEY: ', key, '\tVALUE: ', value)
+        // console.log('KEY: ', key, '\tVALUE: ', value, '\nPATH: ', value?.path)
         if (value?.path) return true
         return false
       })
       .reduce((acc: Endpoint[], [authPath, authEndpoint]) => {
+        // console.log('PATH: ', authEndpoint.path)
         const endpointHandler: PayloadHandler = async (req) => {
+          payloadSingleton(req.payload)
           console.info('ENDPOINT HANDLER FOR: ', authPath)
           return this.betterAuthInstance.handler(req as Request)
         }
 
-        if (typeof authEndpoint.method === 'string') {
+        // console.log(
+        //   authEndpoint.options.method,
+        //   'method typeof ',
+        //   typeof authEndpoint.options.method,
+        // )
+
+        if (typeof authEndpoint.options.method === 'string') {
           acc.push({
             path: `/auth${authEndpoint.path}`,
-            method: authEndpoint.method.toLowerCase(),
+            method: authEndpoint.options.method.toLowerCase(),
             handler: endpointHandler,
           } as Endpoint)
         }
 
-        if (Array.isArray(authEndpoint.method)) {
-          for (const method of authEndpoint.method) {
+        if (Array.isArray(authEndpoint.options.method)) {
+          for (const method of authEndpoint.options.method) {
             acc.push({
               path: `/auth${authEndpoint.path}`,
               method: method.toLowerCase(),
