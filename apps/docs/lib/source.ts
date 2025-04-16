8298b4b1bb6625e1850527bf8235284b8a30ff7f
Ben <b3nab@users.noreply.github.com>
2025-04-16 09:18:48 +0200

    fix (docs): add icon to source

--- apps/docs/lib/source.ts
+++ apps/docs/lib/source.ts
@@ -1,9 +1,20 @@
-import { docs } from '@/.source';
-import { loader } from 'fumadocs-core/source';
+import { createElement } from 'react'
+import { loader } from 'fumadocs-core/source'
+import { icons } from 'lucide-react'
+import { docs } from '@/.source'
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
-});
+  icon(icon): any {
+    if (!icon) {
+      // You may set a default icon
+      return
+    }
+
+    // @ts-ignore
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons])
+  },
+})
