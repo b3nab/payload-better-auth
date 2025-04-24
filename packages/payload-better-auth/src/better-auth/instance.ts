f4be20514771aa68b5c07e60a1fac28056261dc7
Ben <b3nab@users.noreply.github.com>
2025-04-24 19:41:54 +0200

    feat: instance strongly typed (former typescript gymnastic)

--- packages/payload-better-auth/src/better-auth/instance.ts
+++ packages/payload-better-auth/src/better-auth/instance.ts
@@ -12,15 +12,16 @@ export type InferBetterAuthInstance<O extends BetterAuthPluginOptions> =
   // & ImprovedAuth<O>
   // ReturnType<typeof betterAuth<ReturnType<typeof buildBetterAuthOptions<O>>>> & {
   // @ts-ignore
-  ReturnType<typeof betterAuth<BuildBetterAuthOptionsReturnType<O>>> & {
-    // Add a type assertion to help TypeScript understand that the api object can contain any plugin's API methods
-    // HACK: this is a hack to make the api object contain any plugin's API methods and allow the code to compile.
-    // But for some issues on some plugins, those api endpoints are not correctly inferred.
-    // The issues are only on some plugins, not all.
-    // Like the stripe plugin: the `stripeWebhook`method is correctly inferred, but the `cancelSubscription` and all other methods are not.
-    // And the admin plugin: the `banUser` method is not correctly inferred (banUser is an example, all the other methods are not correctly inferred as well).
-    api: Record<string, any>
-  }
+  ReturnType<typeof betterAuth<BuildBetterAuthOptionsReturnType<O>>>
+// & {
+//   // Add a type assertion to help TypeScript understand that the api object can contain any plugin's API methods
+//   // HACK: this is a hack to make the api object contain any plugin's API methods and allow the code to compile.
+//   // But for some issues on some plugins, those api endpoints are not correctly inferred.
+//   // The issues are only on some plugins, not all.
+//   // Like the stripe plugin: the `stripeWebhook`method is correctly inferred, but the `cancelSubscription` and all other methods are not.
+//   // And the admin plugin: the `banUser` method is not correctly inferred (banUser is an example, all the other methods are not correctly inferred as well).
+//   api: Record<string, any>
+// }
 
 // The following error is due to the fact that the types of better-auth are not well designed.
 // The types should be improved to allow for a more type-safe usage of the library.
