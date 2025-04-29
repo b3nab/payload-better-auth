e6125cdd3a17c690b38ef5d3033c6c9fdf85608f
Ben <b3nab@users.noreply.github.com>
2025-04-29 15:20:47 +0200

    feat: improve guards return type

--- packages/payload-better-auth/src/nextjs/guards/guard.type.ts
+++ packages/payload-better-auth/src/nextjs/guards/guard.type.ts
@@ -1,6 +1,30 @@
-import type { SanitizedConfig, User } from 'payload'
+import type { SanitizedConfig } from 'payload'
 import type { GuardServerBefore } from '../server.before.js'
 import type { BetterAuthPluginOptions } from '../../types.js'
+import type { InferInternalBetterAuthInstance } from 'src/better-auth/instance.js'
+
+// declare module 'payload' {
+//   export interface User extends UserAny {}
+// }
+
+// export interface UserAny {
+//   [x: string]: any
+// }
+
+// type User = import('payload').User
+// export type Session = InferInternalBetterAuthInstance['$Infer']['Session']['session']
+// export type User = InferInternalBetterAuthInstance['$Infer']['Session']['user']
+
+// @ts-ignore
+export type Session = import('payload').GeneratedTypes['collections']['session']
+// @ts-ignore
+export type User = import('payload').GeneratedTypes['user']
+// type SelectFromUser = import('payload').TypedCollectionSelect['user']
+// export type User = import('payload').TransformCollectionWithSelect<
+//   'user',
+//   SelectFromUser
+// >
+// export type User = import('payload').TypedUser
 
 export type GuardBuilder<O extends BetterAuthPluginOptions, Args = void> = (
   configPromise: Promise<SanitizedConfig>,
@@ -13,13 +37,13 @@ export type Guard<Args = void> = Args extends void
   : (args: Args, redirectUrl?: string) => Promise<GuardReturn>
 
 type GuardReturn =
-  | {
-      hasSession: true
-      session: any
-      user: any
-    }
-  | {
-      hasSession: false
-      session?: any
-      user?: any
-    }
+  // | {
+  //     hasSession: true
+  //     session: any
+  //     user: User
+  //   }
+  {
+    hasSession: boolean
+    session?: Session
+    user?: User
+  }
