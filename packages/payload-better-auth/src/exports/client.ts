aee0e27de60a273b2c14a2b5257e95b4a7456fd7
Ben <b3nab@users.noreply.github.com>
2026-01-03 00:44:10 +0100

    feat: add TwoFactorAccountButton and SocialLoginButtons exports for enhanced user authentication experience

--- packages/payload-better-auth/src/exports/client.ts
+++ packages/payload-better-auth/src/exports/client.ts
@@ -3,3 +3,9 @@
 export * from '../components/providers/BetterAuthProvider.client.js'
 export * from '../components/views/two-factor-setup/FormSetupTwoFactor.client.js'
 export * from '../components/views/two-factor-verify/FormVerifyTwoFactor.client.js'
+
+// Account page 2FA button
+export { TwoFactorAccountButton } from '../components/views/account/TwoFactorAccountButton.client.js'
+
+// Login View - Social Login Buttons
+export { SocialLoginButtons } from '../components/views/login/SocialLoginButtons.client.js'
--- packages/payload-better-auth/src/exports/rsc.ts
+++ packages/payload-better-auth/src/exports/rsc.ts
@@ -6,13 +6,7 @@ export { BetterAuthServerWrapper } from '../components/providers/BetterAuthProvi
 // Server Views
 export { SetupTwoFactorServer } from '../components/views/two-factor-setup/SetupTwoFactor.server.js'
 export { VerifyTwoFactorServer } from '../components/views/two-factor-verify/VerifyTwoFactor.server.js'
-// export { Account as AccountServer } from '../views/Account/index.js'
-// export { CreateFirstUserView as CreateFirstUserServer } from '../views/CreateFirstUser/index.js'
-// export { ForgotPasswordView as ForgotServer } from '../views/ForgotPassword/index.js'
-// export { LogoutInactivity as InactivityServer } from '../views/Logout/index.js'
-// export { LoginView as LoginServer } from '../views/Login/index.js'
-// export { LogoutView as LogoutServer } from '../views/Logout/index.js'
-// export { ResetPassword as ResetServer } from '../views/ResetPassword/index.js'
-// export { UnauthorizedView as UnauthorizedServer } from '../views/Unauthorized/index.js'
+export { TwoFactorSetupPromptServer } from '../components/views/two-factor-prompt/TwoFactorSetupPrompt.server.js'
 
-// Server Components
+// Login View - Social Login Buttons
+export { SocialLoginButtonsServer } from '../components/views/login/SocialLoginButtons.server.js'
