f797971ae639c6fa1d67bb079c835eb25f495f77
Ben <b3nab@users.noreply.github.com>
2026-07-04 16:30:26 +0200

    fix(demo): preview route request typing

--- apps/demo/src/app/(frontend)/next/preview/route.ts
+++ apps/demo/src/app/(frontend)/next/preview/route.ts
@@ -1,20 +1,13 @@
 import type { CollectionSlug, PayloadRequest } from 'payload'
 import { getPayload } from 'payload'
 
+import type { NextRequest } from 'next/server'
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import configPromise from '@payload-config'
 
-export async function GET(
-  req: {
-    cookies: {
-      get: (name: string) => {
-        value: string
-      }
-    }
-  } & Request,
-): Promise<Response> {
+export async function GET(req: NextRequest): Promise<Response> {
   const payload = await getPayload({ config: configPromise })
 
   const { searchParams } = new URL(req.url)
@@ -25,7 +18,9 @@ export async function GET(
   const previewSecret = searchParams.get('previewSecret')
 
   if (previewSecret !== process.env.PREVIEW_SECRET) {
-    return new Response('You are not allowed to preview this page', { status: 403 })
+    return new Response('You are not allowed to preview this page', {
+      status: 403,
+    })
   }
 
   if (!path || !collection || !slug) {
@@ -33,26 +28,34 @@ export async function GET(
   }
 
   if (!path.startsWith('/')) {
-    return new Response('This endpoint can only be used for relative previews', { status: 500 })
-  }
-
-  let user
-
-  try {
-    user = await payload.auth({
-      req: req as unknown as PayloadRequest,
-      headers: req.headers,
-    })
-  } catch (error) {
-    payload.logger.error({ err: error }, 'Error verifying token for live preview')
-    return new Response('You are not allowed to preview this page', { status: 403 })
+    return new Response(
+      'This endpoint can only be used for relative previews',
+      { status: 500 },
+    )
   }
 
   const draft = await draftMode()
 
-  if (!user) {
-    draft.disable()
-    return new Response('You are not allowed to preview this page', { status: 403 })
+  try {
+    const user = await payload.auth({
+      req: req as unknown as PayloadRequest,
+      headers: req.headers,
+    })
+
+    if (!user) {
+      draft.disable()
+      return new Response('You are not allowed to preview this page', {
+        status: 403,
+      })
+    }
+  } catch (error) {
+    payload.logger.error(
+      { err: error },
+      'Error verifying token for live preview',
+    )
+    return new Response('You are not allowed to preview this page', {
+      status: 403,
+    })
   }
 
   // You can add additional checks here to see if the user is allowed to preview this page
