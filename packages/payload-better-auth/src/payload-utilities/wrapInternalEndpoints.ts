40fc35d6d55ccbab33cbbf17331213669ace3026
Ben <b3nab@users.noreply.github.com>
2026-07-08 16:54:44 +0200

    refactor: remove payload singleton dependency

--- packages/payload-better-auth/src/payload-utilities/wrapInternalEndpoints.ts
+++ packages/payload-better-auth/src/payload-utilities/wrapInternalEndpoints.ts
@@ -1,26 +1,14 @@
-import type { Endpoint, PayloadHandler } from 'payload'
+import type { Endpoint } from 'payload'
 import { addDataAndFileToRequest, addLocalesToRequestFromData } from 'payload'
-
-import { payloadSingleton } from '../singleton.payload.js'
 
 export const wrapInternalEndpoints = (endpoints: Endpoint[]): Endpoint[] => {
   return endpoints.map((endpoint) => {
     const handler = endpoint.handler
-    // let handler: PayloadHandler = endpoint.handler
-
-    if (['get'].includes(endpoint.method)) {
-      endpoint.handler = async (req) => {
-        console.log('method', endpoint.method)
-        payloadSingleton(req.payload)
-        return handler(req)
-      }
-    }
 
     if (['patch', 'post'].includes(endpoint.method)) {
       endpoint.handler = async (req) => {
         await addDataAndFileToRequest(req)
         addLocalesToRequestFromData(req)
-        payloadSingleton(req.payload)
         return handler(req)
       }
     }
