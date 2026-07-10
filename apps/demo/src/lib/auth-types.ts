818805fe0a578d7e82032fd89169b20ade61770f
Ben <b3nab@users.noreply.github.com>
2026-07-10 15:04:49 +0200

    refactor(demo): get auth types from better-auth client

--- apps/demo/src/lib/auth-types.ts
+++ apps/demo/src/lib/auth-types.ts
@@ -1,8 +1,5 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import { auth } from './auth'
-// biome-ignore lint/style/useImportType: <explanation>
-import { client } from './auth-client'
+import type { client } from './auth-client'
 
-export type Session = typeof auth.$Infer.Session
+export type Session = typeof client.$Infer.Session
 export type ActiveOrganization = typeof client.$Infer.ActiveOrganization
 export type Invitation = typeof client.$Infer.Invitation
